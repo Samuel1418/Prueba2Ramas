43749c1ad5b81ed8bfe05253bf9bb480eeb4511a
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2018-11-13 08:57:28 +0100

    Agregar suma

--- src/prueba2ramas/ClaseB.java
+++ src/prueba2ramas/ClaseB.java
@@ -5,10 +5,23 @@
  */
 package prueba2ramas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Samuel
  */
 public class ClaseB {
+    public ClaseB(){}
     
+    public void suma(){
+        Scanner ler= new Scanner(System.in);
+        float sumar1, sumar2, resultado;
+            System.out.println("Introduzca numero1 a sumar");
+            sumar1=ler.nextFloat();
+            System.out.println("Introduzca numero2 a sumar");
+            sumar2=ler.nextFloat();
+            resultado=sumar1+sumar2;
+            System.out.println("El resultado de la suma es= "+resultado);
+    }
 }
